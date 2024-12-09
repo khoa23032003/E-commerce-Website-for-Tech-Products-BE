8fd872f46146ae9b93e10fd5b5a7678b11588cf6
Pontax <toan30112003@gmail.com>
2024-12-09 12:21:58 +0700

    fix jwt

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,19 +1,36 @@
-import { Injectable } from '@nestjs/common';
+//src/auth/jwt.strategy.ts
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { jwtSecret } from './auth.module';
+import { UserService } from '../user/user.service';
 import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
+  constructor(private readonly userService: UserService) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: Request) =>
-          request?.cookies?.jwt ||
-          request?.headers?.authorization?.split(' ')[1],
+        (request: Request) => {
+          const tokenFromCookie = request?.cookies?.jwt;
+          const tokenFromHeader =
+            request?.headers?.authorization?.split(' ')[1];
+          return tokenFromCookie || tokenFromHeader || null;
+        },
       ]),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: jwtSecret,
     });
+  }
+
+  async validate(payload: any) {
+    //console.log('JWT Payload:', payload);
+    const { email } = payload;
+    const user = await this.userService.findByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Prisma, User } from '@prisma/client';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
+  jwtService: any;
   constructor(private prisma: PrismaService) { }
 
   // Đăng ký người dùng
@@ -16,48 +17,47 @@ export class UserService {
   }
 
   // Tìm người dùng qua email
-  async findByEmail(email: string): Promise<User | null> {
-    return this.prisma.user.findUnique({
-      where: { email },
+  async findByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
     });
+    return user;
   }
 
 
   // async login(loginUserDto: LoginUserDto) {
   //   const { email, password } = loginUserDto;
 
-  //   // Xác thực người dùng
-  //   const user = await this.validateUser(email, password);
-  //   if (!user) {
-  //     throw new UnauthorizedException('Invalid email or password');
-  //   }
-  //   // Tạo JWT token
-  //   const payload = { email: user.email, sub: user.id };
-  //   return {
-  //     access_token: this.jwtService.sign(payload),
-  //     user,
-  //   };
-  // }
+  // Xác thực người dùng
+  const user = await this.validateUser(email, password);
 
-  findAll() {
-    return `This action returns all user`;
-  }
+}
+validateUser(email: string, password: string) {
+  throw new Error('Method not implemented.');
+}
+
+findAll() {
+  return `This action returns all user`;
+}
 
 
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+update(id: number, updateUserDto: UpdateUserDto) {
+  return `This action updates a #${id} user`;
+}
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+remove(id: number) {
+  return `This action removes a #${id} user`;
+}
   }
   // Tìm người dùng qua ID
-  async findById(id: string): Promise<User | null> {
-    console.log('id', id);
-    return this.prisma.user.findUnique({
-      where: { id },
-    });
+  async findById(id: string): Promise < User | null > {
+  console.log('id', id);
+  return this.prisma.user.findUnique({
+    where: { id },
+  });
 
-  }
+}
 }
